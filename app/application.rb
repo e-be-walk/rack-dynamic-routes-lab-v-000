88a5b44cf078896796ea58e5f3654a36c8bedd93
Erin Walker <erinb0317@hotmail.com>
2018-03-22 00:03:14 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,13 +9,13 @@ class Application
       if @@items.detect{|i| i.name == item_name}
         item = @@items.detect{|i| i.name == item_name}
         resp.write "You requested this item:#{item_name}"
-        resp.write item.price 
-        resp.status = 200 
+        resp.write item.price
+        resp.status = 200
       else 
         resp.write "Item not found"
-        resp.status = 400 
-      end 
-      
+        resp.status = 400
+      end
+
     else
       resp.write "Route not found"
       resp.status = 404
